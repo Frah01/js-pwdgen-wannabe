fc244733a3fb57ee73d85976d7f0d12029de52f4
Frah01 <vinario.francesco@gmail.com>
2022-11-25 10:01:23 +0100

    Aggiunti " ; "

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 
 //INSERISCI IL NOME
 
-let user_name= prompt('Inserisci il tuo nome')
+let user_name= prompt('Inserisci il tuo nome');
 console.log (user_name)
 
 //INSERISCI IL TUO COGNOME
 
-let user_last_name= prompt('Inserisci il tuo cognome')
+let user_last_name= prompt('Inserisci il tuo cognome');
 console.log (user_last_name)
 
 //INSERISCI IL TUO COLORE PREFERITO
 
-let user_fav_color= prompt ('Inserisci il tuo colore preferito')
+let user_fav_color= prompt ('Inserisci il tuo colore preferito');
 console.log (user_fav_color)
 
 //CONCATENO I VALORI DELLE VARIABILI
 
-let conc= user_name + user_last_name + user_fav_color + 21
+let conc= user_name + user_last_name + user_fav_color + 21;
 console.log (conc)
 
 //MOSTRO A SCHERMO LA PASSWORD
 
-document.getElementById('password').innerHTML= ('La tua password è' + " " + conc)
+document.getElementById('password').innerHTML= ('La tua password è' + " " + conc);
 console.log('La tua password è' + " " + conc)
